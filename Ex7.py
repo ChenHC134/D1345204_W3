92d4c1a289e40b288a489caa78453f22abed09bb
ChenHC134 <d1345204@o365.fcu.edu.tw>
2024-09-26 11:50:05 +0800

    update Ex7.py file

--- Ex7.py
+++ Ex7.py
@@ -1,3 +1,3 @@
 print("複製程式碼")
 n = int(input('輸⼊⼀個⼗進制數字：'))
-print('⼆進制：{:b}'.format(n, 'b'),'八進制：{:o}'.format(n, 'o'),'⼗六進制：{:x}'.format(n, 'x'),sep='\n')
+print('⼆進制：{:b}'.format(n, 'b'),'八進制：{:o}'.format(n, 'o'),'⼗六進制：{:X}'.format(n, 'x'),sep='\n')
